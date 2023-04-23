38f9f14432c978873e0bb8ec37a75fe4846f8153
sanskarram981 <sanskarram992@gmail.com>
2023-04-24 02:10:24 +0530

    changes

--- src/main/java/org/springcore/App.java
+++ src/main/java/org/springcore/App.java
@@ -1,9 +1,8 @@
 package org.springcore;
-
 import org.springcore.collections.Emp;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springcore.ref.*;
 import java.io.IOException;
 
 public class App
@@ -17,9 +16,18 @@ public class App
         System.out.println(st2);
         Student st3 = (Student) context.getBean("student3");
         System.out.println(st3);
+
         ApplicationContext context1 = new ClassPathXmlApplicationContext("collectionconfig.xml");
         Emp em1 = (Emp) context1.getBean("employee");
         System.out.println(em1);
+
+        ApplicationContext context2 = new ClassPathXmlApplicationContext("refconfig.xml");
+        A a1 = (A)context2.getBean("aref");
+        System.out.println(a1);
+        A a2 = (A)context2.getBean("aref1");
+        System.out.println(a2);
+        A a3 = (A)context2.getBean("aref2");
+        System.out.println(a3);
 
     }
 }
---
+++ src/main/java/org/springcore/ref/B.java
@@ -0,0 +1,30 @@
+package org.springcore.ref;
+
+public class B
+{
+    private int y;
+
+    public B()
+    {
+        super();
+    }
+    public B(int y) {
+        super();
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "B{" +
+                "y=" + y +
+                '}';
+    }
+}
